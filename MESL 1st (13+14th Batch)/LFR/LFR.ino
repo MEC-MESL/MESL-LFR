84d1e27f0127b9e9a0325c9742cf7b2054939fe2
Masfiqun <masfiqunahmednooneknows@gmail.com>
2024-10-30 17:34:31 +0600

    Journey Starts

---
+++ MESL 1st (13+14th Batch)/LFR/LFR.ino
@@ -0,0 +1,35 @@
+int motorR = 4;
+int motorL = 5;
+
+int ir1= 6;
+int ir2=7;
+int ir3=8;
+int ir4=9;
+int ir5=10;
+int ir6=11;
+int ir7=12;
+int ir=13
+
+
+
+
+void setup() {
+  
+  pinMode(motorR,OUTPUT);
+  pinMode(motorL,OUTPUT);
+
+  pinMode(ir1,INPUT);
+  pinMode(ir2,INPUT);
+  pinMode(ir3,INPUT);
+  pinMode(ir4,INPUT);
+  pinMode(ir5,INPUT);
+  pinMode(ir6,INPUT);
+  pinMode(ir7,INPUT);
+  pinMode(ir8,INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
